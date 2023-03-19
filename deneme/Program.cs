8ec19c366b54d71d3907b45c1124e63415f21102
Hakanakssy <hakan.aksoy@galata.edu.tr>
2023-03-19 19:42:27 +0300

    let's talk about sharks

---
+++ deneme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace denem
+{
+    class Program
+{
+    
+// See https://aka.ms/new-console-template for more information
+ static void Main(string[] args)
+{
+        Console.WriteLine("Adınız Giriniz: ");
+        string name =Console.ReadLine();
+        Console.WriteLine("Soyadınız Giriniz: ");
+        string surname =Console.ReadLine();
+
+        Console.WriteLine($"merhaba {name} {surname}");
+}
+}
+}
+
