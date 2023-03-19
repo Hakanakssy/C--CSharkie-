0e699a5f350895e280a5b59e5e195ad5e88991fd
Hakanakssy <hakan.aksoy@galata.edu.tr>
2023-03-20 00:03:47 +0300

    kafayı yiyiorum

--- type_convertions/Program.cs
+++ type_convertions/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+    string user_name= "", user_sur="", user_pas="1234",log_pass="";
+    
+    for (int i = 0; i < 5; i++)
+    {
+        user_name=Console.ReadLine();
+        user_sur=Console.ReadLine();
+        log_pass=Console.ReadLine();
+        if (log_pass.Equals(user_pas))
+        {
+            System.Console.WriteLine("Sisteme hoşgeldin");
+            break;
+        }
+        else
+        {
+            System.Console.WriteLine("giriş başarısız");
+            
+        }
+    }
