1c882466456686ae4dcfcd14bc73259014f3d4a7
Hakanakssy <hakan.aksoy@galata.edu.tr>
2023-03-26 16:30:48 +0300

    ıdk

---
+++ myclass/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace myclass;
+public class Class1
+{
+
+}
--- type_convertions/Program.cs
+++ type_convertions/Program.cs
@@ -1,21 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-    string user_name= "", user_sur="", user_pas="1234",log_pass="";
-    
-    for (int i = 0; i < 5; i++)
+﻿namespace type_convertions
+{
+    class Program
     {
-        user_name=Console.ReadLine();
-        user_sur=Console.ReadLine();
-        log_pass=Console.ReadLine();
-        if (log_pass.Equals(user_pas))
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("Sisteme hoşgeldin");
-            break;
-        }
-        else
-        {
-            System.Console.WriteLine("giriş başarısız");
             
         }
     }
+}
